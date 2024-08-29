71cff7f4752160b450081d5516d0372ee90f7799
Ronak010325 <ronakyadav1325@gmail.com>
2024-08-29 22:15:43 +0530

    optimizing javascript Code

--- index.js
+++ index.js
@@ -2,37 +2,41 @@ var randomLeft = Math.floor(Math.random() * 6) + 1;
 var randomRight = Math.floor(Math.random() * 6) + 1;
 
 function loadleft() {
-    if(randomLeft == 1) {
-        document.querySelector(".img1").setAttribute("src","./images/dice1.png");
-    } else if (randomLeft == 2) {
-        document.querySelector(".img1").setAttribute("src","./images/dice2.png");
-    } else if (randomLeft == 3) {
-        document.querySelector(".img1").setAttribute("src","./images/dice3.png");
-    } else if (randomLeft == 4) {
-        document.querySelector(".img1").setAttribute("src","./images/dice4.png");
-    } else if (randomLeft == 5) {
-        document.querySelector(".img1").setAttribute("src","./images/dice5.png");
-    } else {
-        document.querySelector(".img1").setAttribute("src","./images/dice6.png");
-    }
-    console.log(randomLeft);
+    // if(randomLeft == 1) {
+    //     document.querySelector(".img1").setAttribute("src","./images/dice1.png");
+    // } else if (randomLeft == 2) {
+    //     document.querySelector(".img1").setAttribute("src","./images/dice2.png");
+    // } else if (randomLeft == 3) {
+    //     document.querySelector(".img1").setAttribute("src","./images/dice3.png");
+    // } else if (randomLeft == 4) {
+    //     document.querySelector(".img1").setAttribute("src","./images/dice4.png");
+    // } else if (randomLeft == 5) {
+    //     document.querySelector(".img1").setAttribute("src","./images/dice5.png");
+    // } else {
+    //     document.querySelector(".img1").setAttribute("src","./images/dice6.png");
+    // }
+    // console.log(randomLeft);
+    var pathleft = "./images/dice"+randomLeft+".png";
+    document.querySelector(".img1").setAttribute("src",pathleft);
 }
 
 function loadright() {
-    if(randomRight == 1) {
-        document.querySelector(".img2").setAttribute("src","./images/dice1.png");
-    } else if (randomRight == 2) {
-        document.querySelector(".img2").setAttribute("src","./images/dice2.png");
-    } else if (randomRight == 3) {
-        document.querySelector(".img2").setAttribute("src","./images/dice3.png");
-    } else if (randomRight == 4) {
-        document.querySelector(".img2").setAttribute("src","./images/dice4.png");
-    } else if (randomRight == 5) {
-        document.querySelector(".img2").setAttribute("src","./images/dice5.png");
-    } else {
-        document.querySelector(".img2").setAttribute("src","./images/dice6.png");
-    }
-    console.log(randomRight);
+    // if(randomRight == 1) {
+    //     document.querySelector(".img2").setAttribute("src","./images/dice1.png");
+    // } else if (randomRight == 2) {
+    //     document.querySelector(".img2").setAttribute("src","./images/dice2.png");
+    // } else if (randomRight == 3) {
+    //     document.querySelector(".img2").setAttribute("src","./images/dice3.png");
+    // } else if (randomRight == 4) {
+    //     document.querySelector(".img2").setAttribute("src","./images/dice4.png");
+    // } else if (randomRight == 5) {
+    //     document.querySelector(".img2").setAttribute("src","./images/dice5.png");
+    // } else {
+    //     document.querySelector(".img2").setAttribute("src","./images/dice6.png");
+    // }
+    // console.log(randomRight);
+    var pathright = "./images/dice"+randomRight+".png";
+    document.querySelector(".img2").setAttribute("src",pathright);
 }
 
 function winner() {
